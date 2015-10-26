36d111658e00f12ed54c16274af7d783826a2c5c
aboyce <apboyce@outlook.com>
2015-10-26 22:45:34 +0000

    *Added Console Project to test communication with the TFS Server. *Added a working test that talks to TFS and gets information on the latest commit.

---
+++ Prototypes/ConnectingToTFS/ClientObjectModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+
+namespace ConnectingToTFS
+{
+    class ClientObjectModel
+    {
+        public void LatestCommit(Uri uri)
+        {
+            TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(uri, new TfsClientCredentials());
+
+            var vcs = tfs.GetService<VersionControlServer>();
+            var latestChangeId = vcs.GetLatestChangesetId();
+            var latestChangeSet = vcs.GetChangeset(latestChangeId);
+
+            Console.WriteLine("Latest Changeset: {0}", latestChangeId);
+            Console.WriteLine("Committer: {0}", latestChangeSet.CommitterDisplayName);
+            Console.WriteLine("Comment: {0}", latestChangeSet.Comment);
+        }
+    }
+}
---
+++ Prototypes/ConnectingToTFS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectingToTFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = (args.Length < 1) ? new Uri("http://192.168.1.102:8080/tfs/DefaultCollection/") : new Uri(args[0]);
+
+            ClientObjectModel com = new ClientObjectModel();
+
+            com.LatestCommit(uri);
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
