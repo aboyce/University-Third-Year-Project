4ed21333007a322ab195758ddc823138f65b81ef
aboyce <apboyce@outlook.com>
2015-10-26 23:38:15 +0000

    *Added and tested a REST API example that will get the JSON from TFS.

--- Prototypes/ConnectingToTFS/Program.cs
+++ Prototypes/ConnectingToTFS/Program.cs
@@ -10,20 +10,31 @@ namespace ConnectingToTFS
     {
         static void Main(string[] args)
         {
-            Uri uri = (args.Length < 1) ? new Uri("http://192.168.1.102:8080/tfs/DefaultCollection/") : new Uri(args[0]);
 
+            Console.WriteLine("*********************************************************");
+            Console.WriteLine("CLIENT OBJECT MODEL");
             ClientObjectModel com = new ClientObjectModel();
 
-            com.LatestCommit(uri);
+            Console.WriteLine("Latest Commit");
+            Console.WriteLine("---------------------------------------------------------");
+            Uri comUri = (args.Length < 1) ? new Uri("http://192.168.1.102:8080/tfs/DefaultCollection/") : new Uri(args[0]);
+            com.LatestCommit(comUri);
+            Console.WriteLine("---------------------------------------------------------");
 
+            
+            Console.WriteLine("*********************************************************");
+            Console.WriteLine("REST API");
+            RestApi restApi = new RestApi();
 
-
-
-
-
-
+            Console.WriteLine("Changesets From the Last Week");
+            Console.WriteLine("---------------------------------------------------------");
+            string restUri = "http://192.168.1.102:8080/tfs/defaultcollection/_apis/tfvc/changesets?fromDate=" +
+                          DateTime.Now.AddDays(-7).ToString("yyyy-M-d");
+            restApi.LatestCommit(restUri);
+            Console.WriteLine("---------------------------------------------------------");
 
             Console.ReadLine();
+
         }
     }
 }
