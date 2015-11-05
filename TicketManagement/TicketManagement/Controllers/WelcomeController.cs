2ee14be53986484fb3c8325cf968691153c83f14
Adam Boyce <apboyce@outlook.com>
2015-11-05 18:53:04 +0000

    *Updated the Project, including Boostrap. *Added a Welcome Controller and Index page.

---
+++ TicketManagement/TicketManagement/Controllers/WelcomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TicketManagement.Models;
+
+namespace TicketManagement.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Welcome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- TicketManagement/TicketManagement/Models/IdentityModels.cs
+++ TicketManagement/TicketManagement/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace TicketManagement.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<TicketManagement.Models.ApplicationUser> ApplicationUsers { get; set; }
     }
 }
