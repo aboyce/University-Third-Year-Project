86dbb6d8aafcf65d7f085799b6b7980632538b8c
Adam Boyce <apboyce@outlook.com>
2015-11-05 19:58:01 +0000

    *Added full screen image to the Welcome page. *Added JavaScript and Image directories.

---
+++ TicketManagement/TicketManagement/Scripts/Site/Welcome/welcome.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    $(window).load(function () {
+
+        var _window = $(window),
+            $bg = $("#bg_image"),
+            aspectRatio = $bg.width() / $bg.height();
+
+        function resize_bg_image() {
+
+            if ((_window.width() / _window.height()) < aspectRatio) {
+
+                $bg.removeClass().addClass('bg_image_height');
+
+            } else {
+
+                $bg.removeClass().addClass('bg_image_width');
+            }
+
+        }
+
+        _window.resize(resize_bg_image).trigger("resize");
+
+    });
+})();
+
+
+
