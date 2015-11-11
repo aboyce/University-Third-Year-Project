05d0bdb0ef4959c2e03c95446fa255e44161f978
aboyce <apboyce@outlook.com>
2015-11-11 18:10:44 +0000

    *Tested Login and Register (separate pages), and modified change password to follow the same theme.

--- TicketManagement/TicketManagement/App_Start/RouteConfig.cs
+++ TicketManagement/TicketManagement/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TicketManagement
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Welcome", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
