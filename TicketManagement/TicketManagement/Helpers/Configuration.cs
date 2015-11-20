2d634fac944f94fcb20f9a5cd72f46c56d82b910
aboyce <apboyce@outlook.com>
2015-11-20 21:08:16 +0000

    *Added logic to Configuration to get the values from the Web.Config.

--- TicketManagement/TicketManagement/Helpers/Configuration.cs
+++ TicketManagement/TicketManagement/Helpers/Configuration.cs
@@ -8,5 +8,31 @@ namespace TicketManagement.Helpers
 {
     public static class Configuration
     {
+        public static TicketConfiguration GetTicketConfiguration()
+        {
+            TicketConfiguration tconf = new TicketConfiguration();
+            return tconf.Populate() ? tconf : null;
+        }
+    }
+
+    public class TicketConfiguration
+    {
+        public TimeSpan TimeSpanGreen { get; private set; }
+        public TimeSpan TimeSpanAmber { get; private set; }
+        public TimeSpan TimeSpanRed { get; private set; }
+
+        public bool Populate()
+        {
+            try
+            {
+                TimeSpanGreen = TimeSpan.FromMinutes(int.Parse(System.Configuration.ConfigurationManager.AppSettings["TicketTimeSpanGreen"]));
+                TimeSpanAmber = TimeSpan.FromMinutes(int.Parse(System.Configuration.ConfigurationManager.AppSettings["TicketTimeSpanAmber"]));
+                TimeSpanRed = TimeSpan.FromMinutes(int.Parse(System.Configuration.ConfigurationManager.AppSettings["TicketTimeSpanRed"]));
+            }
+            catch
+            { return false; }
+
+            return true;
+        }
     }
 }
