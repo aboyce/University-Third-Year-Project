22fb5e9d72850d9a314b8cebb836bf107eb79610
aboyce <apboyce@outlook.com>
2015-12-05 17:53:41 +0000

    *Added basic CRUD to the majority of the entities.

---
+++ TicketManagement/TicketManagement/Controllers/TicketCategoriesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TicketManagement.Models.Context;
+using TicketManagement.Models.Entities;
+
+namespace TicketManagement.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class TicketCategoriesController : Controller
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        // GET: TicketCategories
+        public ActionResult Index()
+        {
+            var ticketCategories = db.TicketCategories.Include(t => t.Project);
+            return View(ticketCategories.ToList());
+        }
+
+        // GET: TicketCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketCategory ticketCategory = db.TicketCategories.Find(id);
+            if (ticketCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketCategory);
+        }
+
+        // GET: TicketCategories/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Name");
+            return View();
+        }
+
+        // POST: TicketCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,ProjectId,LastUpdated")] TicketCategory ticketCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TicketCategories.Add(ticketCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Name", ticketCategory.ProjectId);
+            return View(ticketCategory);
+        }
+
+        // GET: TicketCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketCategory ticketCategory = db.TicketCategories.Find(id);
+            if (ticketCategory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Name", ticketCategory.ProjectId);
+            return View(ticketCategory);
+        }
+
+        // POST: TicketCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,ProjectId,LastUpdated")] TicketCategory ticketCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticketCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Name", ticketCategory.ProjectId);
+            return View(ticketCategory);
+        }
+
+        // GET: TicketCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketCategory ticketCategory = db.TicketCategories.Find(id);
+            if (ticketCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketCategory);
+        }
+
+        // POST: TicketCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TicketCategory ticketCategory = db.TicketCategories.Find(id);
+            db.TicketCategories.Remove(ticketCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TicketManagement/TicketManagement/Controllers/TicketStatesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TicketManagement.Models.Context;
+using TicketManagement.Models.Entities;
+
+namespace TicketManagement.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class TicketStatesController : Controller
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        // GET: TicketStates
+        public ActionResult Index()
+        {
+            return View(db.TicketStates.ToList());
+        }
+
+        // GET: TicketStates/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketState ticketState = db.TicketStates.Find(id);
+            if (ticketState == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketState);
+        }
+
+        // GET: TicketStates/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TicketStates/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Colour,LastUpdated")] TicketState ticketState)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TicketStates.Add(ticketState);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ticketState);
+        }
+
+        // GET: TicketStates/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketState ticketState = db.TicketStates.Find(id);
+            if (ticketState == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketState);
+        }
+
+        // POST: TicketStates/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Colour,LastUpdated")] TicketState ticketState)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticketState).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ticketState);
+        }
+
+        // GET: TicketStates/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketState ticketState = db.TicketStates.Find(id);
+            if (ticketState == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketState);
+        }
+
+        // POST: TicketStates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TicketState ticketState = db.TicketStates.Find(id);
+            db.TicketStates.Remove(ticketState);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
