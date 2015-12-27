b058ea398204ff3b8b580a6dffe91acb0eb23869
aboyce <apboyce@outlook.com>
2015-12-27 21:55:47 +0000

    *Added Solution to recieve a HTTP request and can see the raw data. **Currently not really usuable, just a proof of concept.

---
+++ ClockworkSMSTextReceiver/ClockworkSMSTextReceiver/Helpers/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockworkSMSTextReceiver.Helpers
+{
+    public enum LogType
+    {
+        Debug = 0,
+        Info = 1,
+        Warning = 2,
+        Error = 3
+    }
+
+    public static class Logger
+    {
+        public static void Log(LogType type, string message)
+        {
+            Console.WriteLine($"{type}: {message}");
+        }
+    }
+}
---
+++ ClockworkSMSTextReceiver/ClockworkSMSTextReceiver/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using ClockworkSMSTextReceiver.Helpers;
+
+namespace ClockworkSMSTextReceiver
+{
+    public class Server
+    {
+        private HttpListener _listener;
+
+        public async void StartServer()
+        {
+            _listener = new HttpListener();
+
+            try
+            {
+                _listener.Prefixes.Add("http://127.0.0.1/");
+
+                Logger.Log(LogType.Info, "Prefix added");
+
+                _listener.Start();
+
+                Logger.Log(LogType.Info, "Listener Started");
+
+                while (_listener.IsListening)
+                {
+                    HttpListenerContext context = await _listener.GetContextAsync();
+
+                    //string rawData = await new StreamReader(context.Request.InputStream, context.Request.ContentEncoding).ReadToEndAsync();
+
+                    string test = HttpUtility.UrlDecode(new StreamReader(context.Request.InputStream, context.Request.ContentEncoding).ReadToEnd());
+
+
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Logger.Log(LogType.Error, e.Message);
+            }
+
+        }
+
+        public void StopServer()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+
+
+    }
+}
