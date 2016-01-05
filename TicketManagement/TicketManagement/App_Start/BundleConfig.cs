4048574c87870964b174d40e68dbfcf011c84681
aboyce <apboyce@outlook.com>
2016-01-05 19:34:03 +0000

    *Added jQuery Unobtrusive AJAX to the project.

--- TicketManagement/TicketManagement/App_Start/BundleConfig.cs
+++ TicketManagement/TicketManagement/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace TicketManagement
             /* ------------------------------------------------- */
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
