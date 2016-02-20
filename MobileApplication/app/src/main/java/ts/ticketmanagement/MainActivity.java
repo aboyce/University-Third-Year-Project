4743c857c790d4abb401669fa036b59df2066d72
aboyce <apboyce@outlook.com>
2016-02-20 17:34:50 +0000

    *Added functionality to make an API call to deactivate the User Token.

--- MobileApplication/app/src/main/java/ts/ticketmanagement/MainActivity.java
+++ MobileApplication/app/src/main/java/ts/ticketmanagement/MainActivity.java
@@ -17,6 +17,8 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -33,8 +35,6 @@ public class MainActivity extends ActivityBase {
     private TextView textViewUsernameValue;
 
     private Intent ticketsIntent;
-    private Intent loginIntent;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +52,7 @@ public class MainActivity extends ActivityBase {
 
         if(!userConfiguredWithApplication()){
             Log.d("TICKET_MANAGEMENT", "MainActivity:onCreate: User not configured with app.");
-            loginIntent = new Intent(this, LoginActivity.class);
-            startActivityForResult(loginIntent, LOGIN_FROM_MAIN);
+            startActivityForResult(new Intent(this, LoginActivity.class), LOGIN_FROM_MAIN);
         } else {
             Log.d("TICKET_MANAGEMENT", "MainActivity:onCreate: User is configured with app.");
 
@@ -119,6 +118,7 @@ public class MainActivity extends ActivityBase {
             String number = getString(R.string.api_phone_numberToConfirm);
             String message = getString(R.string.api_phone_body) + userToken;
             SmsManager.getDefault().sendTextMessage(number, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS Message Sent!", Toast.LENGTH_LONG).show();
             Log.d("TICKET_MANAGEMENT", "MainActivity:trySendSMSMessage: Text message sent.");
             return true;
         }
@@ -169,6 +169,10 @@ public class MainActivity extends ActivityBase {
 
     public void removeUserOnClick(View pView){
         Log.d("TICKET_MANAGEMENT", "MainActivity:removeUserOnClick");
+        new API_DeactivateUserCredentials();
+    }
+
+    private void removeUserFromPhone(){
         SharedPreferences sharedPreferences = this.getSharedPreferences(getString(R.string.persistent_storage_name), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.remove(getString(R.string.persistent_storage_user_username));
@@ -184,8 +188,8 @@ public class MainActivity extends ActivityBase {
         userToken = "";
         textViewUsernameValue.setText(getString(R.string.main_lbl_usernameValue));
 
-        loginIntent = new Intent(this, LoginActivity.class);
-        startActivityForResult(loginIntent, LOGIN_FROM_MAIN);
+        startActivityForResult(new Intent(this, LoginActivity.class), LOGIN_FROM_MAIN);
+        Toast.makeText(getApplicationContext(), "User Token Deactivated from Server and removed from Phone!", Toast.LENGTH_LONG).show();
     }
 
     class API_CheckConnection extends AsyncTask<Void, Void, String> {
@@ -300,6 +304,63 @@ public class MainActivity extends ActivityBase {
             else {
                 showMessageBox("Main", "Cannot Confirm Credentials", "Unfortunately we cannot confirm your credentials, please check your config and try again.");
                 Log.d("TICKET_MANAGEMENT", "LoginActivity-API_ConfirmUserCredentials:onPostExecute: Cannot confirm User credentials");
+            }
+
+            progressbar.setVisibility(View.GONE);
+        }
+    }
+
+    class API_DeactivateUserCredentials extends AsyncTask<Void, Void, String> {
+
+        protected void onPreExecute(){
+            Log.d("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials");
+            progressbar.setVisibility(View.VISIBLE);
+        }
+
+        protected String doInBackground(Void... urls) {
+            Log.d("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials:doInBackground");
+            try{
+                URL url = new URL(getString(R.string.api_url) + getString(R.string.api_user_deactivateUserToken1) + username + getString(R.string.api_user_deactivateUserToken2) + userToken);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                Log.d("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials: Opened HTTP URL Connection to; " + url.toString());
+                try{
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while((line = bufferedReader.readLine()) != null)
+                        stringBuilder.append(line).append("\n");
+                    bufferedReader.close();
+                    Log.d("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials:doInBackground: Response from API=" + stringBuilder.toString());
+                    return stringBuilder.toString();
+                }catch (Exception e){
+                    Log.e("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials:doInBackground: Error: " + e.getMessage(), e);
+                    return null;
+                }
+                finally {
+                    urlConnection.disconnect();
+                }
+            }catch (Exception e){
+                Log.e("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials:doInBackground: Error: " + e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response){
+            Log.d("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials:onPostExecute");
+            if(response == null || response.contains("null")){
+                showMessageBox("Main", "Error Deactivating Credentials", "An error has occurred trying to deactivate your user token, please check the state on the Web Application.");
+                Log.e("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials:onPostExecute: Error: " + response);
+                return;
+            }
+
+            response = response.replace("\"","");
+            Log.d("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials:onPostExecute: Response=" + response);
+
+            if(Objects.equals(response, "true") || response.contains("true"))
+                removeUserFromPhone();
+            else {
+                showMessageBox("Main", "Cannot Deactivate Credentials", "Unfortunately we cannot deactivate your User Token, please check your config and try again.");
+                Log.d("TICKET_MANAGEMENT", "LoginActivity-API_DeactivateUserCredentials:onPostExecute: Cannot deactivate User Token");
             }
 
             progressbar.setVisibility(View.GONE);
