572f65d548e4fce42cf09b7af0cbf27ccf41832e
Adam Boyce <apboyce@outlook.com>
2016-03-21 11:42:43 +0000

    *Added a Testing Project to test the Web API Controllers.

---
+++ TicketManagement/TicketManagement.Tests/Controllers/API/BaseApiUnitTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicketManagement.Tests.Controllers.API
+{
+    [TestClass]
+    public class BaseApiUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
