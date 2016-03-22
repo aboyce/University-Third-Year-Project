3c481ced090261957eb1f9081f7b4e4277f95785
Adam Boyce <apboyce@outlook.com>
2016-03-22 18:06:41 +0000

    *Added test to check CheckUserToken.

--- TicketManagement/TicketManagement.Tests/Controllers/API/UserControllerTests.cs
+++ TicketManagement/TicketManagement.Tests/Controllers/API/UserControllerTests.cs
@@ -41,7 +41,7 @@ namespace TicketManagement.Tests.Controllers.API
             await Database.SaveChangesAsync();
 
             // Test that the controller prevents us from continuing when the User is not supposed to be used.
-            if(await controllerUnderTest.GetNewUserToken(user.Id) != null)
+            if(await controllerUnderTest.GetNewUserToken(user.UserName) != null)
                 Assert.Fail("Controller did not prevent progression when the User was not in the correct state.");
 
             // Setup.
@@ -57,6 +57,49 @@ namespace TicketManagement.Tests.Controllers.API
                 Assert.Fail("JsonResult ContentType not as expected.");
             if (result.Data == null)
                 Assert.Fail("JsonResult Data not as expected.");
+        }
+
+        [TestMethod]
+        public async Task Test_UserController_CheckUserToken()
+        {
+            // Setup.
+            SeedDatabase();
+            UserController controllerUnderTest = new UserController(Database);
+
+            // Check that the controller handles incorrect paramters.
+            // These should be returning 'false'.
+            if (await controllerUnderTest.CheckUserToken(null, null))
+                Assert.Fail("Controller not handling incorrect parameters.");
+            if (await controllerUnderTest.CheckUserToken("", ""))
+                Assert.Fail("Controller not handling incorrect parameters.");
+            // Test it can handle incorrect Users as parameters
+            if (await controllerUnderTest.CheckUserToken("this_is_not_a_username_that_should_be_in_the_db", "this_is_not_a_usertoken_that_should_be_in_the_db"))
+                Assert.Fail("Controller not handling incorrect parameters.");
+
+            // Check that we are set up as expected for the test.
+            List<User> users = Database.Users.ToList();
+            if (users.Count != 2)
+                Assert.Fail("Seeded database not as expected for this test.");
+
+            // Setup (A User does not have a mobile setup and has a new, valid User Token.).
+            User user = users.First();
+            user.MobileApplicationConfirmed = false;
+            await controllerUnderTest.GetNewUserToken(user.UserName); // This should work, as we have independently tested it.
+            Database.Entry(user).State = EntityState.Modified;
+            await Database.SaveChangesAsync();
+
+            // Test that controller doesn't give us back an incorrect response (with the MobileApplicationConfirmed).
+            if (await controllerUnderTest.CheckUserToken(user.UserName, user.UserToken))
+                Assert.Fail("The controller should not have said that the token was correct, as the MobileApplicaitonConfirmed should be false.");
+
+            // Setup.
+            user.MobileApplicationConfirmed = true;
+            Database.Entry(user).State = EntityState.Modified;
+            await Database.SaveChangesAsync();
+
+            // Test that the controller prevents us from continuing when the User is not supposed to be used.
+            if (!await controllerUnderTest.CheckUserToken(user.UserName, user.UserToken))
+                Assert.Fail("Controller should have confirmed that the passed in User Token is valid.");
         }
 
 
