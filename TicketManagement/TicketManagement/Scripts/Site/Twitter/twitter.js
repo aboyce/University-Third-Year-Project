d2ddfb9bb257806a314985f9a792e5e0469f8cc2
Adam Boyce <apboyce@outlook.com>
2016-03-26 21:25:48 +0000

    *Added working buttons for the Twitter Timeline, that will dynamically display depending on requirements and will properly load the child comment if present. *Functionality for creating the Tweet into a Ticket works. *Funtionality to reply to a Tweet appears and needs completing. **Finally!

--- TicketManagement/TicketManagement/Scripts/Site/Twitter/twitter.js
+++ TicketManagement/TicketManagement/Scripts/Site/Twitter/twitter.js
@@ -3,3 +3,8 @@
     //$('#twitter_home_timeline_form').submit();
     $('#twitter_user_timeline_form').submit();
 })();
+
+//$('#modal_twitter_reply').on('show.bs.modal', function (e) {
+//    var model = $(this), tweetId = e.relatedTarget.id;
+//    model.find('.edit-content').html(tweetId);
+//})
